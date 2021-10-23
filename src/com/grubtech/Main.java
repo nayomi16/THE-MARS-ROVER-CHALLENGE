d481f772f4b73b6987db1b119ed901e1c1278290
nayomi16 <nayomirashmikaaa@gmail.com>
2021-10-23 13:37:57 +0530

    Getting input from the user

--- src/com/grubtech/Main.java
+++ src/com/grubtech/Main.java
@@ -1,9 +1,27 @@
 package com.grubtech;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("test");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the Coordinates of the Plateau : X Y");
+        String coordinates = scan.nextLine();
+
+        int i = 1;
+        while(true)
+        {
+            System.out.println("Enter the Rover "+i+" position : X Y P");
+            String roverPosition = scan.nextLine();
+
+            System.out.println("Enter the Rover "+i+" explore instructions : LMMLMR");
+            String exploreInstructions = scan.nextLine();
+            i++;
+
+
+        }
+
     }
 }
